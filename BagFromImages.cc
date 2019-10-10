8495857972d84320027aa7a7d65af20e78d168d5
jagat <nir.j@husky.neu.edu>
2019-10-09 21:40:43 -0400

    corrected data types to vector of strings and tested

--- BagFromImages.cc
+++ BagFromImages.cc
@@ -12,21 +12,24 @@
 #include <boost/algorithm/string.hpp>
 #include "Thirdparty/DLib/FileFunctions.h"
 #define SEC_TO_MICROSECS 1000000
-#define SECS_TO_NANCOSECS 1000000000
+#define SEC_TO_NANOSECS 1000000000
 
 enum timeAccuracy
 {   MICROSECONDS = 0,
     NANOSECONDS = 1
 };
 using namespace std;
+ros::Time convert_to_ros_time(uint64_t timeStamp,timeAccuracy t);
+std::vector<std::string> split(const std::string& s, char delimiter);
+uint64_t extract_time_from_filename(std::string filename,int location_in_string);
 
 int main(int argc, char **argv)
 {
     ros::init(argc, argv, "BagFromImages");
 
-    if(argc!=5)
+    if(argc!=4)
     {
-        cerr << "Usage: rosrun BagFromImages BagFromImages <path to image directory> <image extension .ext> <frequency> <path to output bag>" << endl;
+        cerr << "Usage: rosrun BagFromImages BagFromImages <path to image directory> <image extension .ext>  <path to output bag>" << endl;
         return 0;
     }
 
@@ -45,12 +48,10 @@ int main(int argc, char **argv)
     //double freq = atof(argv[3]);
 
     // Output bag
-    rosbag::Bag bag_out(argv[4],rosbag::bagmode::Write);
+    rosbag::Bag bag_out(argv[3],rosbag::bagmode::Write);
 
-    ros::Time t = ros::Time::now();
-
-    const float T=1.0f/freq;
-    ros::Duration d(T);
+    //ros::Time t = ros::Time::now();
+    cout<<"initialisation done"<<endl;
 
     for(size_t i=0;i<filenames.size();i++)
     {
@@ -58,10 +59,10 @@ int main(int argc, char **argv)
             break;
         // Get time stamp from filename
         uint64_t timeStampOfImage = extract_time_from_filename(filenames[i],4);
-        ros::Time rosTimeStamp = convert_to_ros_time(timeStampOfImage,timeAccuracy.MICROSECONDS)
+        ros::Time rosTimeStamp = convert_to_ros_time(timeStampOfImage,timeAccuracy::MICROSECONDS);
 
-        cout<<"The time stamp is: "<<to_string(timeStampOfImage)<<endl;
-        ROS_INFO_STREAM<<"ROS Time stamp is : "<<rosTimeStamp;
+        //cout<<"The time stamp is: "<<to_string(rosTimeStamp.sec)<<endl;
+        //ROS_INFO_STREAM<<"ROS Time stamp is : "<<rosTimeStamp;
 
         // store the sensor message in cvImage
         cv::Mat im = cv::imread(filenames[i],cv::IMREAD_GRAYSCALE);
@@ -82,25 +83,32 @@ int main(int argc, char **argv)
 
 ros::Time convert_to_ros_time(uint64_t timeStamp,timeAccuracy t)
 {
-    if(t == timeAccuracy.MICROSECONDS)
+    ros::Time rosTimeStamp;
+    if(t == timeAccuracy::MICROSECONDS)
     {
         uint32_t seconds = (timeStamp/SEC_TO_MICROSECS);
-        uint32_t micro_seconds = (timeStamp%SEC_TO_MICROSECSSEC_TO_MICROSECS);
-        ros::Time rosTimeStamp(seconds,microseconds*1000);
+        uint32_t microseconds = (timeStamp%SEC_TO_MICROSECS);
+        rosTimeStamp = ros::Time(seconds,microseconds*1000);
     }
-    else if (t == timeAccuracy.NANOSECONDS)
+    else if (t == timeAccuracy::NANOSECONDS)
     {
         uint32_t seconds = (timeStamp/SEC_TO_NANOSECS);
-        uint32_t nano_seconds = (timeStamp%SEC_TO_NANOSECS);
-        ros::Time rosTimeStamp(seconds,nano_seconds);
+        uint32_t nanoseconds = (timeStamp%SEC_TO_NANOSECS);
+        rosTimeStamp = ros::Time(seconds,nanoseconds);
     }
+    return rosTimeStamp;
 }
 
 uint64_t extract_time_from_filename(std::string filename,int location_in_string)
 {
-    filename_without_extension = split(filename,'.');
-    filename_parts = split(filename_without_extension,'_');
-    uint64_t timeStampFromImageName = stol(filename_parts[location_in_string];
+    std::vector<std::string> filename_without_extension = split(filename,'.');
+    cout<<"filename without extension:"<<filename_without_extension.at(0)<<" and file extension: "<<filename_without_extension.at(1)<<endl;
+    std::vector<std::string> filename_parts = split(filename_without_extension.at(0),'_');
+    cout<<"Image sequence from filename:"<<filename_parts.at(3)<< " and its timestamp : "<<filename_parts.at(4)<<endl;
+    std::string::size_type sz;
+    uint64_t timeStampFromImageName = std::stol(filename_parts.at(location_in_string),&sz);
+
+    return timeStampFromImageName;
 }
 
 std::vector<std::string> split(const std::string& s, char delimiter)
